14aaea9d4ec9e05e2cc6da2df7b4513bb20c326f
EzequielRomio <romio.ezequiel@gmail.com>
2021-07-22 19:55:19 -0300

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Redirect, Switch } from 'react-router';
 
+import HomePage from './components/HomePage';
+import DetailsPage from './components/DetailsPage';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Switch>
+        <Route exact path={'/'} component={HomePage} />
+        <Route exact path={'/details/:id'} component={DetailsPage}/>
+        <Route path={'*'}>
+          <Redirect to={'/'}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
